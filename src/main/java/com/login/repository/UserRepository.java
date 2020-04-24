7ed780725d63c568d8fa88ea070eb4d4bcaafe9f
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-04-24 14:07:06 -0700

    Created user service

--- src/main/java/com/login/repository/UserRepository.java
+++ src/main/java/com/login/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.login.repository;
 
-public class UserRepository {
+import com.login.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//userRepository is extending to Jparepository to inherit it's functions
+public interface UserRepository extends JpaRepository<User, Long > {
+    User findByEmail(String email);
+    User findByUserName(String userName);
 }
