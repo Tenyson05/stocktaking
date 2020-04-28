e0082480fae5f8501f4083866de48bb4db9beb16
Rojahog@hotmail.com <rojahog@Hotmail.com>
2020-04-28 15:41:32 -0700

    Updating the Crud methods to a more up to date format

--- src/main/java/com/login/controller/LoginController.java
+++ src/main/java/com/login/controller/LoginController.java
@@ -7,8 +7,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
@@ -18,14 +18,14 @@ public class LoginController {
 
     @Autowired
     private UserService userService;
-    @RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+    @GetMapping(value={"/", "/login"})
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("login");
         return  modelAndView;
     }
 
-    @RequestMapping(value="/registration", method = RequestMethod.GET)
+    @GetMapping(value="/registration")
     public ModelAndView registration() {
         ModelAndView modelAndView = new ModelAndView();
         User user = new User();
@@ -34,7 +34,7 @@ public class LoginController {
         return modelAndView;
     }
 
-    @RequestMapping(value="registration", method = RequestMethod.POST)
+    @PostMapping(value="/registration")
     public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
         ModelAndView modelAndView = new ModelAndView();
         User userExists = userService.findUserByUserName(user.getUserName());
@@ -52,7 +52,7 @@ public class LoginController {
         return modelAndView;
     }
 
-    @RequestMapping(value="/admin/home", method = RequestMethod.GET)
+    @GetMapping(value="/admin/home")
     public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
